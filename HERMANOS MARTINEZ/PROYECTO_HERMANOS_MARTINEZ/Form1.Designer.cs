68d4509afaf513813c20a8eb2356ea45c2234a34
gortz19 <137531145+gortz19@users.noreply.github.com>
2023-11-12 18:48:50 -0600

    Add files via upload

---
+++ HERMANOS MARTINEZ/PROYECTO_HERMANOS_MARTINEZ/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace PROYECTO_HERMANOS_MARTINEZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.REPORTESBTN = new System.Windows.Forms.Button();
+            this.VALESBTN = new System.Windows.Forms.Button();
+            this.FACTURASBTN = new System.Windows.Forms.Button();
+            this.GENERAR_REPORTESNTN = new System.Windows.Forms.Button();
+            this.ASIGNAR_VIAJESBTN = new System.Windows.Forms.Button();
+            this.LIBROS_GENERALESBTN = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.REGISTRAR_VIAJESBTN = new System.Windows.Forms.Button();
+            this.REGISTRAR_EMPLEADOSBTN = new System.Windows.Forms.Button();
+            this.REGISTRO_VEHICULOSBTN = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1911, 1042);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Location = new System.Drawing.Point(351, 134);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1545, 901);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.AutoSize = true;
+            this.panel3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.REPORTESBTN);
+            this.panel3.Controls.Add(this.VALESBTN);
+            this.panel3.Controls.Add(this.FACTURASBTN);
+            this.panel3.Controls.Add(this.GENERAR_REPORTESNTN);
+            this.panel3.Controls.Add(this.ASIGNAR_VIAJESBTN);
+            this.panel3.Controls.Add(this.LIBROS_GENERALESBTN);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1911, 132);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // REPORTESBTN
+            // 
+            this.REPORTESBTN.AutoSize = true;
+            this.REPORTESBTN.BackColor = System.Drawing.Color.Black;
+            this.REPORTESBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.REPORTESBTN.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.REPORTESBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.REPORTESBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REPORTESBTN.ForeColor = System.Drawing.Color.White;
+            this.REPORTESBTN.Location = new System.Drawing.Point(1639, 42);
+            this.REPORTESBTN.Name = "REPORTESBTN";
+            this.REPORTESBTN.Size = new System.Drawing.Size(240, 54);
+            this.REPORTESBTN.TabIndex = 8;
+            this.REPORTESBTN.Text = "REPORTES";
+            this.REPORTESBTN.UseVisualStyleBackColor = false;
+            // 
+            // VALESBTN
+            // 
+            this.VALESBTN.AutoSize = true;
+            this.VALESBTN.BackColor = System.Drawing.Color.Black;
+            this.VALESBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.VALESBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VALESBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VALESBTN.ForeColor = System.Drawing.Color.White;
+            this.VALESBTN.Location = new System.Drawing.Point(1393, 42);
+            this.VALESBTN.Name = "VALESBTN";
+            this.VALESBTN.Size = new System.Drawing.Size(240, 54);
+            this.VALESBTN.TabIndex = 7;
+            this.VALESBTN.Text = "VALES";
+            this.VALESBTN.UseVisualStyleBackColor = false;
+            // 
+            // FACTURASBTN
+            // 
+            this.FACTURASBTN.AutoSize = true;
+            this.FACTURASBTN.BackColor = System.Drawing.Color.Black;
+            this.FACTURASBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.FACTURASBTN.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FACTURASBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FACTURASBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FACTURASBTN.ForeColor = System.Drawing.Color.White;
+            this.FACTURASBTN.Location = new System.Drawing.Point(1147, 42);
+            this.FACTURASBTN.Name = "FACTURASBTN";
+            this.FACTURASBTN.Size = new System.Drawing.Size(240, 54);
+            this.FACTURASBTN.TabIndex = 6;
+            this.FACTURASBTN.Text = "FACTURAS";
+            this.FACTURASBTN.UseVisualStyleBackColor = false;
+            // 
+            // GENERAR_REPORTESNTN
+            // 
+            this.GENERAR_REPORTESNTN.AutoSize = true;
+            this.GENERAR_REPORTESNTN.BackColor = System.Drawing.Color.Black;
+            this.GENERAR_REPORTESNTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.GENERAR_REPORTESNTN.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.GENERAR_REPORTESNTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GENERAR_REPORTESNTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GENERAR_REPORTESNTN.ForeColor = System.Drawing.Color.White;
+            this.GENERAR_REPORTESNTN.Location = new System.Drawing.Point(865, 42);
+            this.GENERAR_REPORTESNTN.Name = "GENERAR_REPORTESNTN";
+            this.GENERAR_REPORTESNTN.Size = new System.Drawing.Size(276, 54);
+            this.GENERAR_REPORTESNTN.TabIndex = 5;
+            this.GENERAR_REPORTESNTN.Text = "GENERAR REPORTES";
+            this.GENERAR_REPORTESNTN.UseVisualStyleBackColor = false;
+            // 
+            // ASIGNAR_VIAJESBTN
+            // 
+            this.ASIGNAR_VIAJESBTN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ASIGNAR_VIAJESBTN.AutoSize = true;
+            this.ASIGNAR_VIAJESBTN.BackColor = System.Drawing.Color.Black;
+            this.ASIGNAR_VIAJESBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ASIGNAR_VIAJESBTN.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ASIGNAR_VIAJESBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ASIGNAR_VIAJESBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ASIGNAR_VIAJESBTN.ForeColor = System.Drawing.Color.White;
+            this.ASIGNAR_VIAJESBTN.Location = new System.Drawing.Point(596, 42);
+            this.ASIGNAR_VIAJESBTN.Name = "ASIGNAR_VIAJESBTN";
+            this.ASIGNAR_VIAJESBTN.Size = new System.Drawing.Size(263, 54);
+            this.ASIGNAR_VIAJESBTN.TabIndex = 4;
+            this.ASIGNAR_VIAJESBTN.Text = "ASIGNACION DE VIAJES";
+            this.ASIGNAR_VIAJESBTN.UseVisualStyleBackColor = false;
+            // 
+            // LIBROS_GENERALESBTN
+            // 
+            this.LIBROS_GENERALESBTN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LIBROS_GENERALESBTN.AutoSize = true;
+            this.LIBROS_GENERALESBTN.BackColor = System.Drawing.Color.Black;
+            this.LIBROS_GENERALESBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.LIBROS_GENERALESBTN.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.LIBROS_GENERALESBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LIBROS_GENERALESBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LIBROS_GENERALESBTN.ForeColor = System.Drawing.Color.White;
+            this.LIBROS_GENERALESBTN.Location = new System.Drawing.Point(350, 42);
+            this.LIBROS_GENERALESBTN.Name = "LIBROS_GENERALESBTN";
+            this.LIBROS_GENERALESBTN.Size = new System.Drawing.Size(240, 54);
+            this.LIBROS_GENERALESBTN.TabIndex = 3;
+            this.LIBROS_GENERALESBTN.Text = "LIBROS GENERALES";
+            this.LIBROS_GENERALESBTN.UseVisualStyleBackColor = false;
+            this.LIBROS_GENERALESBTN.Click += new System.EventHandler(this.LIBROS_GENERALESBTN_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.REGISTRAR_VIAJESBTN);
+            this.panel2.Controls.Add(this.REGISTRAR_EMPLEADOSBTN);
+            this.panel2.Controls.Add(this.REGISTRO_VEHICULOSBTN);
+            this.panel2.Location = new System.Drawing.Point(0, 130);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(346, 912);
+            this.panel2.TabIndex = 1;
+            // 
+            // REGISTRAR_VIAJESBTN
+            // 
+            this.REGISTRAR_VIAJESBTN.BackColor = System.Drawing.Color.DarkOrchid;
+            this.REGISTRAR_VIAJESBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REGISTRAR_VIAJESBTN.ForeColor = System.Drawing.Color.White;
+            this.REGISTRAR_VIAJESBTN.Location = new System.Drawing.Point(23, 345);
+            this.REGISTRAR_VIAJESBTN.Name = "REGISTRAR_VIAJESBTN";
+            this.REGISTRAR_VIAJESBTN.Size = new System.Drawing.Size(289, 68);
+            this.REGISTRAR_VIAJESBTN.TabIndex = 5;
+            this.REGISTRAR_VIAJESBTN.Text = "REGISTRAR VIAJES";
+            this.REGISTRAR_VIAJESBTN.UseVisualStyleBackColor = false;
+            // 
+            // REGISTRAR_EMPLEADOSBTN
+            // 
+            this.REGISTRAR_EMPLEADOSBTN.BackColor = System.Drawing.Color.DarkOrchid;
+            this.REGISTRAR_EMPLEADOSBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REGISTRAR_EMPLEADOSBTN.ForeColor = System.Drawing.Color.White;
+            this.REGISTRAR_EMPLEADOSBTN.Location = new System.Drawing.Point(23, 233);
+            this.REGISTRAR_EMPLEADOSBTN.Name = "REGISTRAR_EMPLEADOSBTN";
+            this.REGISTRAR_EMPLEADOSBTN.Size = new System.Drawing.Size(289, 68);
+            this.REGISTRAR_EMPLEADOSBTN.TabIndex = 4;
+            this.REGISTRAR_EMPLEADOSBTN.Text = "REGISTRAR EMPLEADOS";
+            this.REGISTRAR_EMPLEADOSBTN.UseVisualStyleBackColor = false;
+            // 
+            // REGISTRO_VEHICULOSBTN
+            // 
+            this.REGISTRO_VEHICULOSBTN.BackColor = System.Drawing.Color.DarkOrchid;
+            this.REGISTRO_VEHICULOSBTN.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.REGISTRO_VEHICULOSBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.REGISTRO_VEHICULOSBTN.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REGISTRO_VEHICULOSBTN.ForeColor = System.Drawing.Color.White;
+            this.REGISTRO_VEHICULOSBTN.Location = new System.Drawing.Point(23, 122);
+            this.REGISTRO_VEHICULOSBTN.Name = "REGISTRO_VEHICULOSBTN";
+            this.REGISTRO_VEHICULOSBTN.Size = new System.Drawing.Size(289, 68);
+            this.REGISTRO_VEHICULOSBTN.TabIndex = 3;
+            this.REGISTRO_VEHICULOSBTN.Text = " REGISTRAR VEHICULOS";
+            this.REGISTRO_VEHICULOSBTN.UseVisualStyleBackColor = false;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "INDEX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button REGISTRO_VEHICULOSBTN;
+        private System.Windows.Forms.Button REGISTRAR_VIAJESBTN;
+        private System.Windows.Forms.Button REGISTRAR_EMPLEADOSBTN;
+        private System.Windows.Forms.Button LIBROS_GENERALESBTN;
+        private System.Windows.Forms.Button ASIGNAR_VIAJESBTN;
+        private System.Windows.Forms.Button GENERAR_REPORTESNTN;
+        private System.Windows.Forms.Button FACTURASBTN;
+        private System.Windows.Forms.Button VALESBTN;
+        private System.Windows.Forms.Button REPORTESBTN;
+        private System.Windows.Forms.Panel panel4;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ HERMANOS MARTINEZ/PROYECTO_HERMANOS_MARTINEZ/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace PROYECTO_HERMANOS_MARTINEZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+   
+            int borderRadius = 6;
+
+            GraphicsPath path1 = new GraphicsPath();
+            path1.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path1.AddArc(LIBROS_GENERALESBTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path1.AddArc(LIBROS_GENERALESBTN.Width - borderRadius * 2, LIBROS_GENERALESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path1.AddArc(0, LIBROS_GENERALESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            LIBROS_GENERALESBTN.Region = new Region(path1);
+
+            GraphicsPath path2 = new GraphicsPath();
+            path2.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path2.AddArc(ASIGNAR_VIAJESBTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path2.AddArc(ASIGNAR_VIAJESBTN.Width - borderRadius * 2, ASIGNAR_VIAJESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path2.AddArc(0, ASIGNAR_VIAJESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            ASIGNAR_VIAJESBTN.Region = new Region(path2);
+
+            GraphicsPath path3 = new GraphicsPath();
+            path3.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path3.AddArc(GENERAR_REPORTESNTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path3.AddArc(GENERAR_REPORTESNTN.Width - borderRadius * 2, VALESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path3.AddArc(0, GENERAR_REPORTESNTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            GENERAR_REPORTESNTN.Region = new Region(path3);
+
+            GraphicsPath path4 = new GraphicsPath();
+            path4.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path4.AddArc(FACTURASBTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path4.AddArc(FACTURASBTN.Width - borderRadius * 2, FACTURASBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path4.AddArc(0, FACTURASBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            FACTURASBTN.Region = new Region(path4);
+
+            GraphicsPath path5 = new GraphicsPath();
+            path5.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path5.AddArc(REPORTESBTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path5.AddArc(REPORTESBTN.Width - borderRadius * 2, REPORTESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path5.AddArc(0, REPORTESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            REPORTESBTN.Region = new Region(path5);
+
+            GraphicsPath path6 = new GraphicsPath();
+            path6.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90); // Esquina superior izquierda
+            path6.AddArc(VALESBTN.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90); // Esquina superior derecha
+            path6.AddArc(VALESBTN.Width - borderRadius * 2, VALESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90); // Esquina inferior derecha
+            path6.AddArc(0, VALESBTN.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90); // Esquina inferior izquierda
+            VALESBTN.Region = new Region(path6);
+
+            int Radius = 8;
+
+            REGISTRAR_EMPLEADOSBTN.FlatStyle = FlatStyle.Flat;
+            REGISTRAR_EMPLEADOSBTN.FlatAppearance.BorderColor = Color.DarkOrchid;
+
+            REGISTRAR_VIAJESBTN.FlatStyle = FlatStyle.Flat;
+            REGISTRAR_VIAJESBTN.FlatAppearance.BorderColor = Color.DarkOrchid;
+
+            GraphicsPath path7 = new GraphicsPath();
+            path7.AddArc(0, 0, Radius * 2, Radius * 2, 180, 90); // Esquina superior izquierda
+            path7.AddArc(REGISTRO_VEHICULOSBTN.Width - Radius * 2, 0, Radius * 2, Radius * 2, 270, 90); // Esquina superior derecha
+            path7.AddArc(REGISTRO_VEHICULOSBTN.Width - Radius * 2, REGISTRO_VEHICULOSBTN.Height - Radius * 2, Radius * 2, Radius * 2, 0, 90); // Esquina inferior derecha
+            path7.AddArc(0, REGISTRO_VEHICULOSBTN.Height - Radius * 2, Radius * 2, Radius * 2, 90, 90); // Esquina inferior izquierda
+            REGISTRO_VEHICULOSBTN.Region = new Region(path7);
+
+            GraphicsPath path8 = new GraphicsPath();
+            path8.AddArc(0, 0, Radius * 2, Radius * 2, 180, 90); // Esquina superior izquierda
+            path8.AddArc(REGISTRAR_EMPLEADOSBTN.Width - Radius * 2, 0, Radius * 2, Radius * 2, 270, 90); // Esquina superior derecha
+            path8.AddArc(REGISTRAR_EMPLEADOSBTN.Width - Radius * 2, REGISTRAR_EMPLEADOSBTN.Height - Radius * 2, Radius * 2, Radius * 2, 0, 90); // Esquina inferior derecha
+            path8.AddArc(0, REGISTRAR_EMPLEADOSBTN.Height - Radius * 2, Radius * 2, Radius * 2, 90, 90); // Esquina inferior izquierda
+            REGISTRAR_EMPLEADOSBTN.Region = new Region(path8);
+
+            GraphicsPath path9 = new GraphicsPath();
+            path9.AddArc(0, 0, Radius * 2, Radius * 2, 180, 90); // Esquina superior izquierda
+            path9.AddArc(REGISTRAR_VIAJESBTN.Width - Radius * 2, 0, Radius * 2, Radius * 2, 270, 90); // Esquina superior derecha
+            path9.AddArc(REGISTRAR_VIAJESBTN.Width - Radius * 2, REGISTRAR_VIAJESBTN.Height - Radius * 2, Radius * 2, Radius * 2, 0, 90); // Esquina inferior derecha
+            path9.AddArc(0, REGISTRAR_VIAJESBTN.Height - Radius * 2, Radius * 2, Radius * 2, 90, 90); // Esquina inferior izquierda
+            REGISTRAR_VIAJESBTN.Region = new Region(path9);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized; // Maximiza la ventana
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal; // Restaura la ventana al estado normal
+            }
+
+
+        }
+
+        
+        private void LIBROS_GENERALESBTN_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
